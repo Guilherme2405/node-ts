bfe91cdd287d0d772c682d08084bb6d22491f2f5
Guilherme Cataneo <guilhermecataneo02@gmail.com>
2025-07-03 13:16:13 -0300

    aplicando .gitignore

--- node001/dist/server.js
+++
@@ -1,9 +0,0 @@
-import { createServer } from "http";
-console.log('Starting server...');
-const server = createServer((req, res) => {
-    return res.end('<h1>csacasca</h1>');
-});
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-//# sourceMappingURL=server.js.map
