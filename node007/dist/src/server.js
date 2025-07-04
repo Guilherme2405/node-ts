e0b5e97da215f553f016680e804f4610b090e8b7
Guilherme Cataneo <guilhermecataneo02@gmail.com>
2025-07-04 15:31:05 -0300

    atc node007

---
+++ node007/dist/src/server.js
@@ -0,0 +1,20 @@
+const users = [];
+import { createServer } from "http";
+console.log('Starting server...');
+const server = createServer((req, res) => {
+    const { method, url } = req;
+    if (method === 'GET' && url === '/users') {
+        return res
+            .setHeader('Content-Type', 'application/json')
+            .end(users ? JSON.stringify(users) : '[]');
+    }
+    if (method === 'POST' && url === '/users') {
+        users.push({ name: 'guiga kirin', email: "guiga@email.com" });
+        return res.writeHead(201).end('<h1>Created</h1>');
+    }
+    return res.writeHead(404).end(TypeError('Not Found').message);
+});
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+//# sourceMappingURL=server.js.map
---
+++ node007/streams/stream-http-server.ts
@@ -0,0 +1,25 @@
+import { createServer } from "node:http";
+import { Transform } from "node:stream";
+
+class inverseNumberStream extends Transform {
+    // cria uma classe extendendo Transform do Node.js
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) * -1;
+
+        console.log(transformed)
+
+        callback(null, Buffer.from(String(transformed)));
+    }
+}
+ const server = createServer(async (req, res) => {
+    const bufffers: Buffer[] = [];
+
+    for await (const chunk of req) {
+        bufffers.push(chunk);
+    }
+    const fulllBody = Buffer.concat(bufffers).toString();
+    console.log(fulllBody + "dados recebidos");
+    return res.end(fulllBody);
+
+});
+server.listen(3333);
